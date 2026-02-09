5419c90c3d9884fc3f855aa8e6b15e7feae29482
Nevindi_Rathukumarage <nevindirasanja@gmail.com>
2026-02-09 23:52:03 +0200

    Created a navigation bar

---
+++ ex2.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <?php include 'header.php'; ?>
+
+<?php
+
+    echo "<h2>The header and footer are visible.</h2>";
+    echo "<h3>Task 2 completed.</h3>";
+
+?>
+</body>
+<?php include 'footer.php'; ?>
+
+</html>
